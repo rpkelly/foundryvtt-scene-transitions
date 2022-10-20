acf0d7b2de96fd4c77ce6e13b362e8fc5de3f764
p4535992 <tentimarco0@gmail.com>
2022-10-20 21:37:24 +0200

    version 0.4.1

--- src/scripts/module.js
+++ src/scripts/module.js
@@ -41,39 +41,40 @@ export const readyHooks = async () => {
 		// game.socket.emit("module.scene-transitions", options);
 		sceneTransitionsSocket.executeForEveryone("executeAction", options);
 	});
-	Hooks.on("closeTransitionForm", (form) => {
-		let activeSceneTransition = form.object;
-		activeSceneTransition.destroy(true);
-		clearInterval(form.interval);
-	});
-	/********************
-	 * Adds menu option to Scene Nav and Directory
-	 *******************/
-	//Credit to Winks' Everybody Look Here for the code to add menu option to Scene Nav
-	Hooks.on("getSceneNavigationContext", (html, contextOptions) => {
-		contextOptions.push(SceneTransition.addPlayTransitionBtn("sceneId"));
-		contextOptions.push(SceneTransition.addCreateTransitionBtn("sceneId"));
-		contextOptions.push(SceneTransition.addEditTransitionBtn("sceneId"));
-		contextOptions.push(SceneTransition.addDeleteTransitionBtn("sceneId"));
-	});
-	Hooks.on("getSceneDirectoryEntryContext", (html, contextOptions) => {
-		contextOptions.push(SceneTransition.addPlayTransitionBtn("documentId"));
-		contextOptions.push(SceneTransition.addCreateTransitionBtn("documentId"));
-		contextOptions.push(SceneTransition.addEditTransitionBtn("documentId"));
-		contextOptions.push(SceneTransition.addDeleteTransitionBtn("documentId"));
-	});
-	Hooks.on("getJournalDirectoryEntryContext", (html, contextOptions) => {
-		contextOptions.push(SceneTransition.addPlayTransitionBtnJE("documentId"));
-	});
-	Hooks.on("renderJournalSheet", (journal) => {
-		if (
-			game.user?.isGM &&
-			$("#" + journal.id + " > header").find(".play-transition").length == 0 &&
-			game.settings.get(CONSTANTS.MODULE_NAME, "show-journal-header-transition") == true
-		) {
-			$('<a class="play-transition"><i class="fas fa-play-circle"></i> Play as Transition</a>').insertAfter(
-				"#" + journal.id + " > header > h4"
-			);
-		}
-	});
 };
+
+Hooks.on("closeTransitionForm", (form) => {
+	let activeSceneTransition = form.object;
+	activeSceneTransition.destroy(true);
+	clearInterval(form.interval);
+});
+/********************
+ * Adds menu option to Scene Nav and Directory
+ *******************/
+//Credit to Winks' Everybody Look Here for the code to add menu option to Scene Nav
+Hooks.on("getSceneNavigationContext", (html, contextOptions) => {
+	contextOptions.push(SceneTransition.addPlayTransitionBtn("sceneId"));
+	contextOptions.push(SceneTransition.addCreateTransitionBtn("sceneId"));
+	contextOptions.push(SceneTransition.addEditTransitionBtn("sceneId"));
+	contextOptions.push(SceneTransition.addDeleteTransitionBtn("sceneId"));
+});
+Hooks.on("getSceneDirectoryEntryContext", (html, contextOptions) => {
+	contextOptions.push(SceneTransition.addPlayTransitionBtn("documentId"));
+	contextOptions.push(SceneTransition.addCreateTransitionBtn("documentId"));
+	contextOptions.push(SceneTransition.addEditTransitionBtn("documentId"));
+	contextOptions.push(SceneTransition.addDeleteTransitionBtn("documentId"));
+});
+Hooks.on("getJournalDirectoryEntryContext", (html, contextOptions) => {
+	contextOptions.push(SceneTransition.addPlayTransitionBtnJE("documentId"));
+});
+Hooks.on("renderJournalSheet", (journal) => {
+	if (
+		game.user?.isGM &&
+		$("#" + journal.id + " > header").find(".play-transition").length == 0 &&
+		game.settings.get(CONSTANTS.MODULE_NAME, "show-journal-header-transition") == true
+	) {
+		$('<a class="play-transition"><i class="fas fa-play-circle"></i> Play as Transition</a>').insertAfter(
+			"#" + journal.id + " > header > h4"
+		);
+	}
+});
